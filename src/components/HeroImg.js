2844b15a282ae440f6e34a13061ab94243953779
gerald <oforige@gmail.com>
2022-05-25 08:35:04 +0000

    completed the landing page for the portfolio sitie

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,12 +2,22 @@ import "./HeroImg.css";
 
 import React from 'react'
 
+import CodeBackground from "../assets/code_background.jpg"
+import { Link } from "react-router-dom";
+
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            
+            <img className="into-img" src={CodeBackground} alt="Background_Image"/>
         </div>
+        <div className="content">
+            <p>Hi I'm a Full-Stack</p>
+            <h1>Web Developer</h1><br/>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn-light">Contact</Link>
+        </div>
+
     </div>
   )
 }
