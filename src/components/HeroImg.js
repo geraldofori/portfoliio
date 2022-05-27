e7f714f5617112b10fab839abd0a047e01c25631
gerald <oforige@gmail.com>
2022-05-27 09:31:49 +0000

    changes done to the title of the app

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -12,7 +12,7 @@ const HeroImg = () => {
             <img className="into-img" src={CodeBackground} alt="Background_Image"/>
         </div>
         <div className="content">
-            <p>Hi I'm a Full-Stack</p>
+            <p>Hi, I'm a Full-Stack</p>
             <h1>Web Developer</h1><br/>
             <Link to="/project" className="btn">Projects</Link>
             <Link to="/contact" className="btn-light">Contact</Link>
