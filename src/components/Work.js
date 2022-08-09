e231c1ae291028de7edcc28041c7e9c405794311
gerald <oforige@gmail.com>
2022-08-09 11:09:36 +0000

    restructuring of the projects components

---
+++ src/components/Work.js
@@ -0,0 +1,18 @@
+import "./WorkCard.css";
+import React from 'react';
+import alphaprep from "../assets/alphaprep.png";
+import { NavLink } from "react-router-dom";
+
+const Work = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Work
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -5,22 +5,16 @@ import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <img src={alphaprep} alt=""/>
-                <h2 className="project-title">AlphaPrep</h2>
-                <div className="pro-details">
-                    <p>This is the project description</p>
-                    <div className="pro-btns">
-                      <NavLink to="github.com" className="btn">View</NavLink>
-                      <NavLink to="github.com" className="btn">Source</NavLink>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+    <div className="project-card">
+      <img src={alphaprep} alt=""/>
+      <h2 className="project-title">AlphaPrep</h2>
+      <div className="pro-details">
+          <p>This is the project description</p>
+          <div className="pro-btns">
+            <NavLink to="https://github.com" className="btn">View</NavLink>
+            <NavLink to="https://github.com" className="btn">Source</NavLink>
+          </div>
+      </div>
     </div>
   )
 }
