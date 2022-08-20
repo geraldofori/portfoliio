4b4c7f5f1cb8355bfd0d00fb35552cc67309a172
gerald <oforige@gmail.com>
2022-08-20 22:11:21 +0000

    finished the contact page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Topbar from '../components/Topbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
       <Topbar/>
       <HeroImg2 heading="CONTACT" text="Get in touch with me on here"/>
+      <Form/>
       <Footer/>
     </div>
   )
